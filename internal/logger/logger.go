b9018667e9cc64f2177765c976a8384f6467cfd1
alexperezortuno <alexperezortuno@gmail.com>
2026-05-05 00:02:33 -0400

    Remove unused context-based logger utilities from `logger` package

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"context"
 	"io"
 	"log/slog"
 	"os"
@@ -43,39 +42,4 @@ func Default() *slog.Logger {
 		defaultLogger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: defaultLevel}))
 	})
 	return defaultLogger
-}
-
-func WithContext(ctx context.Context) context.Context {
-	return WithContextAttrs(Default(), ctx)
-}
-
-func WithContextAttrs(logger *slog.Logger, ctx context.Context) context.Context {
-	return context.WithValue(ctx, loggerKey, logger)
-}
-
-func FromContext(ctx context.Context) *slog.Logger {
-	if logger, ok := ctx.Value(loggerKey).(*slog.Logger); ok {
-		return logger
-	}
-	return Default()
-}
-
-func Debug(msg string, args ...any) {
-	Default().Debug(msg, args...)
-}
-
-func Info(msg string, args ...any) {
-	Default().Info(msg, args...)
-}
-
-func Warn(msg string, args ...any) {
-	Default().Warn(msg, args...)
-}
-
-func Error(msg string, args ...any) {
-	Default().Error(msg, args...)
-}
-
-func Err(err error, msg string, args ...any) {
-	Default().Error(msg, append([]any{"error", err}, args...)...)
 }
